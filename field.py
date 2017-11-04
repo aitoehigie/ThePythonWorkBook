1316327f39d8e984a1b5ed26d72ea20ecc86e2cf
aitoehigie <aitoehigie@gmail.com>
2017-11-04 03:47:22 +0100

    update field solution

--- field.py
+++ field.py
@@ -2,8 +2,8 @@
 
 length: float = float(input("Length of the field in feet:"))
 breadth: float = float(input("Width of the field in feet:"))
-
+square_feet_per_acre = 43560
 area = length * breadth
-area_in_acres = area/43560
+area_in_acres = area/square_feet_per_acre
 
 print(f"The area of the field is {area_in_acres} acres")
