0da181829be295b0f4d5ce44baa639df23c68c60
aitoehigie <aitoehigie@gmail.com>
2017-11-04 03:23:54 +0100

    solution to the area of a room problem

---
+++ area.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+
+length: float = float(input("Length of the room in feet:"))
+breadth: float = float(input("Width of the room in feet:"))
+
+area = length * breadth
+
+print(f"The area of the room is {area} feet")
