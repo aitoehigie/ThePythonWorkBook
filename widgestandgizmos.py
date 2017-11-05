4fbad1ec5c0849a6995eb3b968e06c343d8e1f77
aitoehigie <aitoehigie@gmail.com>
2017-11-05 10:38:21 +0100

    remove the test folder

--- widgestandgizmos.py
+++ widgestandgizmos.py
@@ -6,11 +6,11 @@ gizmos = int(input("Enter the number of gizmos:"))
 weight_of_widget = 75
 weight_of_gizmos = 112
 
-total_weight_of widgets = widgets * weight_of_widget
+total_weight_of_widgets = widgets * weight_of_widget
 total_weight_of_gizmos = gizmos * weight_of_gizmos
 
 total_weight_of_order = total_weight_of_widgets + total_weight_of_gizmos
 
 print(f"""
-Total weight of order = {total_weight_of_order}
+Total weight of order: {total_weight_of_order}
 """)
