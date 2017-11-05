0a64650c9456322044b41547a0520f0951b122f7
aitoehigie <aitoehigie@gmail.com>
2017-11-05 16:44:46 +0100

    update compound interest solution

--- compound_interest.py
+++ compound_interest.py
@@ -9,9 +9,9 @@ third_year_interest = (second_year_interest * 0.04) + second_year_interest
 print(f"""
 Account Statement
 #################################
-1st Year:${first_year_interest}
-2nd Year: ${second_year_interest}
-3rd Year: ${third_year_interest}
+1st Year: ${round(first_year_interest, 2)}
+2nd Year: ${round(second_year_interest, 2)}
+3rd Year: ${round(third_year_interest, 2)}
 """)
 
 
