4a5207c7c4fb2a03a37a66b0aa3a01477e4e7ef3
aitoehigie <aitoehigie@gmail.com>
2017-11-05 14:35:06 +0100

    Solution to arithmetic problem

---
+++ arithmetic.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+import math
+
+a = int(input("enter a value for a:"))
+b = int(input("enter a value for b:"))
+sum = a + b
+difference = b - a
+product = a * b
+quotient = a / b
+remainder = a // b
+log = math.log(a)
+power = math.pow(a, b)
+
+print(f"""
+Sum of {a} and {b}: {sum}
+Difference between {a} and {b}: {difference}
+Product of {a} and {b}: {product}
+Quotient when {a} is divided by {b}: {product}
+Remainder when {a} is divided by {b}: {remainder}
+log10{a}: {log}
+{a} raised to power {b}: {power}
+""")
+
+
+
+
